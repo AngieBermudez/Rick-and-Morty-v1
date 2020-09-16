d4912c4aa1f7c275110f81e24509858027ecaecf
AngieBermudez <cielangie2019@gmail.com>
2020-09-16 18:15:47 -0500

    cambio de fondo, agrege botones y modifique estilos

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function cambiarnombre(idH2){
+    let cambio = prompt('Cambia el nombre de tu personaje')
+    document.getElementById(idH2).innerHTML = cambio
+}
+//cambiarnombre()
+
+/*function sumarNumeros(){
+    var num1 = parseInt(prompt("introduce un numero"))
+    var num2 = parseInt(prompt("Introduce oto numero"))
+
+    var resultado = num1 + num2
+
+    alert(resultado)
+
+
+}
+sumarNumeros()    */
